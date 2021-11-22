acf344c8f8dda2b776e9137919ad7ed2509af4bc
isotkerem <76882982+isotkerem@users.noreply.github.com>
2021-11-22 20:47:15 +0300

    Add files via upload

---
+++ src/config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  owners: [],
+  nodes: [{
+    identifier: 'NODE-MAIN-2',
+    host: 'lavalink-repl.ahmasa.repl.co',
+    password: 'youshallnotpass',
+    port: 443,
+    secure: true,
+  }],
+  prefix: 'nb!',
+  updateSlash: false,
+};
